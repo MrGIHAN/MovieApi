d4ca910860c3739a3055406f6177c9582fac74ba
GIHAN PATHIRANA <gihanpathirana84@gmail.com>
2025-07-05 23:40:08 +0530

    first comiit

---
+++ movie-back-end/src/test/java/dev/gihan/movie/back_end/MovieBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gihan.movie.back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
